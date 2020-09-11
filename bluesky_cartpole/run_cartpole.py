8267e73e63e0083133c4d3a1f46f2dfb10293eaa
jklynch <jlynch@bnl.gov>
2020-09-11 10:39:08 -0400

    add command line arguments use databroker

--- bluesky_cartpole/run_cartpole.py
+++ bluesky_cartpole/run_cartpole.py
@@ -1,3 +1,5 @@
+import argparse
+
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
 from databroker import Broker
@@ -6,18 +8,28 @@ from bluesky_cartpole.cartpole_plan import train_cartpole_agent
 
 
 def run():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--agent-name", required=True, type=str)
+    arg_parser.add_argument("--episode-count", required=True, type=int)
+
+    args = arg_parser.parse_args()
+
     RE = RunEngine()
 
     bec = BestEffortCallback()
 
     RE.subscribe(bec)
 
-    db = Broker.named("temp")
+    db = Broker.named("bluesky-cartpole")
 
     # Insert all metadata/data captured into db.
     RE.subscribe(db.insert)
 
-    RE(train_cartpole_agent(episode_count=1000))
+    RE(
+        train_cartpole_agent(
+            agent_name=args.agent_name, episode_count=args.episode_count
+        )
+    )
 
 
 if __name__ == "__main__":
